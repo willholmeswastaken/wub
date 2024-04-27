42e8f5faa4e572379b7bd51806109b4a08cfe740
Will Holmes <willholmes97@icloud.com>
2024-04-27 20:34:40 +0100

    Basic redirect endpoint done

---
+++ src/app/[...code]/route.ts
@@ -0,0 +1,17 @@
+import { db } from '@/server/db';
+import { links } from '@/server/db/schema';
+import { eq } from 'drizzle-orm';
+import { notFound, redirect } from 'next/navigation'
+ 
+export async function GET(
+    request: Request,
+  { params }: { params: { code: string } }
+) {
+    const route = await db.query.links.findFirst({
+        where: eq(links.short_code, params.code)
+    });
+    if(!route) {
+        redirect('/')
+    }
+  redirect(route.url)
+}
