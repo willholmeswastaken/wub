8a67c63cac3a80b2a99473ae9a0c504778e10a65
Will Holmes <willholmes97@icloud.com>
2024-05-03 18:15:28 +0100

    added some comment detail

--- src/components/short-link.tsx
+++ src/components/short-link.tsx
@@ -1,6 +1,10 @@
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
 
 export default function ShortLink({ url, clicks }: { url: string, clicks: number }) {
+    // make it animate in
+    // add copy button
+    // make clicks look cool with icon graph
+    // add a default favicon
     return (
         <Card>
             <CardHeader className="items-start p-3">
