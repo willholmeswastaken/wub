6c8bf530f427516c2d7494bb47e1d68928f47dbd
Will Holmes <willholmes97@icloud.com>
2024-05-07 18:02:53 +0100

    Added more content to short link component

--- src/components/short-link.tsx
+++ src/components/short-link.tsx
@@ -2,6 +2,7 @@
 
 import { Card, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import { LinkIcon } from "./link-icon";
+import { CopyIcon } from "lucide-react";
 
 export default function ShortLink({ url, clicks, shortUrl }: { url: string, clicks: number, shortUrl: string }) {
     // make it animate in
@@ -9,13 +10,14 @@ export default function ShortLink({ url, clicks, shortUrl }: { url: string, clic
     // make clicks look cool with icon graph
     // add a default favicon
     return (
-        <Card className="hover:border-black cursor-pointer shadow-lg border-gray-200">
-            <CardHeader className="flex flex-row p-4 space-x-2 items-start justify-start space-y-0">
-                <LinkIcon />
-                <div className="flex flex-col items-start justify-start space-y-0">
-                    <CardTitle className="flex flex-row space-x-2">
+        <Card className="hover:border-black duration-75 transition-[border-color] cursor-pointer shadow-lg border-gray-200">
+            <CardHeader className="flex flex-row p-4 space-x-2 items-center justify-start space-y-0">
+                <LinkIcon className="h-10 w-10" />
+                <div className="flex flex-col space-y-0 items-start">
+                    <CardTitle className="flex flex-row space-x-2 items-center">
                         <span>{shortUrl}</span>
                         <span>{clicks}</span>
+                        <button className="bg-gray-100 hover:bg-blue-200 transition-all rounded-full duration-75 hover:scale-105 active:scale-95 p-1.5"><CopyIcon width={14} height={14} className="text-gray-700 transition-all group-hover:text-blue-800" /></button>
                     </CardTitle>
                     <CardDescription>{url}</CardDescription>
                 </div>
