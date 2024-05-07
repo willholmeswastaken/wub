49e39dcc593fe1b91d3ebd1c32e2488d652f74eb
Will Holmes <willholmes97@icloud.com>
2024-05-07 19:14:59 +0100

    Fix up landing ui

--- src/components/why-us.tsx
+++ src/components/why-us.tsx
@@ -2,7 +2,7 @@ import { LinkIcon } from "@/components/link-icon"
 
 export function WhyUs() {
     return (
-        <section className="w-full py-12 md:py-24 lg:py-32">
+        <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
             <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-10">
                 <div className="space-y-3">
                     <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
