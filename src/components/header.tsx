6b129fceb03180a0704508882c80a4bec121f536
Will Holmes <willholmes97@icloud.com>
2024-05-11 11:40:25 +0100

    Added avatar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,21 @@
 import Link from "next/link";
 import { LinkIcon } from "@/components/link-icon";
+import { getServerAuthSession } from "@/server/auth";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-export function Header() {
+export async function Header() {
+    const session = await getServerAuthSession();
+
     return (
         <header className="px-4 lg:px-6 h-14 flex items-center">
             <Link className="flex items-center justify-center" href="#">
                 <LinkIcon className="h-6 w-6" />
                 <span className="sr-only">Link Shortener</span>
             </Link>
+            <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
             {/* <nav className="ml-auto flex gap-4 sm:gap-6">
                 <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
                     Features
