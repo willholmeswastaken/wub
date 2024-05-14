c6d666b7672a8094deb8c856daddddad1e775150
Will Holmes <willholmes97@icloud.com>
2024-05-14 20:34:43 +0100

    Fixed up the dashboard page

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -2,11 +2,11 @@ import { Button } from "@/components/ui/button";
 
 export function AppHeader() {
     return (
-        <section className="flex justify-center items-center border-y border-gray-200 p-10 bg-white">
-            <div className="mx-auto max-w-2xl w-full">
+        <section className="border-y border-gray-200 p-10 bg-white">
+            <div className="mx-auto max-w-3xl w-full flex items-center justify-between">
                 <h1 className="text-2xl text-gray-600">Links</h1>
+                <Button>Create Link</Button>
             </div>
-            <Button>Create New</Button>
         </section>
     );
 }
--- src/components/full-link-card.tsx
+++ src/components/full-link-card.tsx
@@ -1,8 +1,16 @@
+'use client';
+
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { CopyButton } from "@/components/copy-button";
 import { ClicksButton } from "@/components/clicks-button";
 
 export function FullLinkCard({ shortCode, url, clicks, createdAt }: { shortCode: string, url: string, clicks: number, createdAt: Date }) {
+    const formatDate = (date: Date): string => {
+        const options: Intl.DateTimeFormatOptions = { month: 'long', day: 'numeric' };
+        return date.toLocaleDateString('en-US', options);
+    };
+
+    const formattedDate = formatDate(createdAt);
     return (
         <div className="w-full bg-white rounded-lg shadow-lg p-4 border-gray-50 hover:shadow-xl duration-200 transition-all hover:cursor-pointer">
             <div className="flex items-center justify-between relative">
@@ -16,11 +24,11 @@ export function FullLinkCard({ shortCode, url, clicks, createdAt }: { shortCode:
                             <a href="#" className="text-sm font-semibold">wub.sh/{shortCode}</a>
                             <CopyButton
                                 isExpired={false}
-                                text={`wub.sh/${shortCode}`}
+                                text={`${window.location.origin}/${shortCode}`}
                             />
                         </div>
                         <div className="flex max-w-fit flex-wrap items-center gap-x-2 text-sm font-normal text-gray-600">
-                            <span>May 14</span>
+                            <span>{formattedDate}</span>
                             <span>-</span>
                             <a href="#" className="hover:underline">{url}</a>
                         </div>
